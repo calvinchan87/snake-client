d2cd2627e64f530bd5a9a7ec2069224aa7b186f4
Calvin Chan <calvinchan87@gmail.com>
2021-03-12 12:15:33 +0000

    add handleUserInput function to play.js, snake initials to client.js

--- client.js
+++ client.js
@@ -10,12 +10,18 @@ const client = {
       host: 'localhost',
       port: 50541
     });
+
     // interpret incoming data as text
     conn.setEncoding('utf8'); 
-  
+
+    conn.on('connect', () => {
+      console.log('Successfully connected to game server.');
+      conn.write('Name: C87');
+      // setInterval(function() { conn.write('Move: up') }, 50);
+    });
+
     conn.on('data', (data) => {
       console.log('Server says: ', data);
-  
     });
 
   }
